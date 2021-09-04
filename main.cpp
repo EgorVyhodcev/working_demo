993fd0d8911ed7158a2a99c688d49339f4c8ef93
EgorVyhodcev <wf-game-acc@bk.ru>
2021-09-04 15:19:48 +0300

    a-b is now stored in the variable d

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@ using namespace std;
 
 int main()
 {
-    int a, b, s, p;
+    int a, b, s, p, d;
 	cin >> a >> b;
 	s = a + b;
 	cout << s << endl;
 	p = a * b;
 	cout << p << endl;
-	cout << a - b << endl;
+	d = a - b;
+	cout << d << endl;
     return 0;
 }
